df7b675e46d3a27d2bd3f34565765b8cc7724ccf
sinapam <suebvisai.pam@gmail.com>
2023-01-08 23:10:44 +0700

    useCallback for asyncCallback

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -28,7 +28,7 @@ function asyncFetchReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState, dependencies) {
+function useAsync(asyncCallback, initialState) {
   const [state, dispatch] = React.useReducer(asyncFetchReducer, initialState)
 
   React.useEffect(() => {
@@ -46,27 +46,23 @@ function useAsync(asyncCallback, initialState, dependencies) {
         dispatch({type: 'rejected', error})
       },
     )
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies)
+  }, [asyncCallback])
 
   return state
 }
 
 function PokemonInfo({pokemonName}) {
-  const state = useAsync(
-    () => {
-      if (!pokemonName) {
-        return
-      }
-      return fetchPokemon(pokemonName)
-    },
-    {
-      status: pokemonName ? 'pending' : 'idle',
-      data: null,
-      error: null,
-    },
-    [pokemonName],
-  )
+  const pokemonCallback = React.useCallback(() => {
+    if (!pokemonName) {
+      return
+    }
+    return fetchPokemon(pokemonName)
+  }, [pokemonName])
+  const state = useAsync(pokemonCallback, {
+    status: pokemonName ? 'pending' : 'idle',
+    data: null,
+    error: null,
+  })
   const {data, status, error} = state
 
   switch (status) {
